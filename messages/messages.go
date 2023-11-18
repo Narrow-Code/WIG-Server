a3513396be80e8bf51b3ead52d2479a6f9baad16
stitchy <matt.mccaughey91@gmail.com>
2023-11-18 12:10:46 -0500

    #37 add userMessages.go

--- messages/messages.go
+++ messages/messages.go
@@ -4,22 +4,21 @@
 package messages
 
 const (
-	ErrorParsingRequest = "There was an error parsing JSON request." // ErrorParsingRequest is a constant string error message for when the JSON request could not be parsed.
-	ErrorWithConnection = "Connection error" // ErrorWithConnection is a constant string error message for whenever there is a connectivity issue.
-	RecordNotFound = "Record was not found" // RecordNotFound is a constant string error message for when a record is not found within a database.
-	UIDEmpty = "No UID found" // UIDEmpty is a constant string error message for when the UID in a request is empty but required.
-	QRMissing = "No QR found" // QRMissing is a constant string error message for when the QR in a request is empty but required.
-	ItemNotFound = "Item not found in database" // ItemNotFound is a constant string error message for when the item barcode was not found in the database.
+	ErrorParsingRequest = "There was an error parsing JSON request." // Error message for when the JSON request could not be parsed.
+	ErrorWithConnection = "Connection error" // Error message for whenever there is a connectivity issue.
+	RecordNotFound = "Record was not found" // Error message for when a record is not found within a database.
+	UIDEmpty = "No UID found" // Error message for when the UID in a request is empty but required.
+	QRMissing = "No QR found" // Error message for when the QR in a request is empty but required.
+	ItemNotFound = "Item not found in database" // Error message for when the item barcode was not found in the database.
 
+	Location = "LOCATION" // Success message for when the QR code is stored as a location.
+	Ownership = "OWNERSHIP" // Success message for when the QR code is stored as an ownership.
+	New = "NEW" // Success message for when the QR code has not been stored in the database.
 
-	Location = "LOCATION" // Location is a constant string success message for when the QR code is stored as a location.
-	Ownership = "OWNERSHIP" // Ownership is a constant string success message for when the QR code is stored as an ownership.
-	New = "NEW" // New is a constant string success message for when the QR code has not been stored in the database.
-
-	ConversionError = "There was an error converting from string to int" // UIDConversionError is a constant string error message for when there was an error message converting string to int
-	NegativeError = "Cannot pass a negative argument" // NegativeError is a constant string error message for when a negative argument is passed as a parameter.
-	BarcodeIntError = "Barcode must be of int value" // BarcodeIntError is a constant string error message for when a non-int value is passed to the barcode argument.
-	ErrorDeletingOwnership = "There was an error deleting ownership" // ErrorDeletingOwnership is a constant string errormessage for when there was an error deleting an ownership from the database.
-	DoesNotExist = " does not exist" // DoesNotExist is a constant string error message, meant to be appended after a field type to show that it the field does not exist.
-	RecordInUse = " is in use" // RecordInUse is a constant string error message, meant to be appended after a field type to show that a field is already in use.
+	ConversionError = "There was an error converting from string to int" // Error message for when there was an error message converting string to int
+	NegativeError = "Cannot pass a negative argument" // Error message for when a negative argument is passed as a parameter.
+	BarcodeIntError = "Barcode must be of int value" // Error message for when a non-int value is passed to the barcode argument.
+	ErrorDeletingOwnership = "There was an error deleting ownership" // Error message for when there was an error deleting an ownership from the database.
+	DoesNotExist = " does not exist" // Error message, meant to be appended after a field type to show that it the field does not exist.
+	RecordInUse = " is in use" // Error message, meant to be appended after a field type to show that a field is already in use.
 )
--- messages/userMessages.go
+++ messages/userMessages.go
@@ -4,22 +4,26 @@
 package messages
 
 const (
-	TokenEmpty = "No token found" // TokenEmpty is a constant string error message for when the token in a request is empty but required.
-	AccessDenied = "Unauthorized" // AccessDenied is a constant string error message for when the access was denied to make the API request.
-	UsernameDoesNotExist = "Username does not exist" // UsernameDoesNotExist is a constant string error message for when an invalid username is being passed in a request.
-	UsernamePasswordDoNotMatch = "Username and password do not match" // UsernamePasswordDoNotMatch is a constant string error message for when the username does not match the hash in the request.
-	UserLoginSuccess = "User log in success" // UserLoginSuccess is a constant string success message for when the username and hash match in the requests.
-	SaltReturned = "Salt was returned" // SaltReturned is a constant string success message for when the users matching salt was successfully found in the database and returned.
-	ErrorToken = "Access denied by token" // ErrorToken is a constant string error message for when the users stored token does not match their UID in a request.
-	TokenPass = "Token authenticated" // TokenPass is a constant string success message for when the users stored token matches their UID in a request.
-	UsernameEmpty = "Username is required" // UsernameEmpty is a constant string error message for when the username is missing in a request.
-	UsernameInUse = "Username is already in use" // UsernameInUse is a constant string error message for when the username in the request is already in use.
-	EmailInUse = "Email associated with another account" // EmailInUse is a constant string error message for when the email in the request is already in use.
-	EmailEmpty = "Email is required" // EmailEmpty is a constant string error message for when the email is missing in a request.
-	SaltMissing = "Salt is missing" // SaltMissing is a constant string error message for when the salt is missing in a request.
-	HashMissing = "Hash is missing" // HashMissing is a constant string error message for when the hash is missing in a request.
-	SignupSuccess = "User added successfully" // SignupSuccess is a constant string success message for when the user is successesfully added to the database.
-	ErrorUsernameRequirements = "Username does not match criteria" // ErrorUsernameRequirements is a constant string error message for when the username in the request does not match the criteria.
-	ErrorEmailRequirements = "Not a valid email address" // ErrorEmailRequirements is a constant string error message for when the email in the request does not match the criteria.
+	TokenEmpty = "No token found" // Error message for when the token in a request is empty but required.
+	ErrorToken = "Access denied by token" // Error message for when the users stored token does not match their UID in a request.
+	TokenPass = "Token authenticated" // Success message for when the users stored token matches their UID in a request.
+	AccessDenied = "Unauthorized" // Error message for when the access was denied to make the API request.
+
+	UsernameDoesNotExist = "Username does not exist" // Error message for when an invalid username is being passed in a request.
+	UsernameEmpty = "Username is required" // Error message for when the username is missing in a request.
+	UsernameInUse = "Username is already in use" // Error message for when the username in the request is already in use.
+	ErrorUsernameRequirements = "Username does not match criteria" // Error message for when the username in the request does not match the criteria.
+	UsernamePasswordDoNotMatch = "Username and password do not match" // Error message for when the username does not match the hash in the request.
+
+	EmailInUse = "Email associated with another account" // Error message for when the email in the request is already in use.
+	EmailEmpty = "Email is required" // Error message for when the email is missing in a request.
+	ErrorEmailRequirements = "Not a valid email address" // Error message for when the email in the request does not match the criteria.
+
+	SaltMissing = "Salt is missing" // Error message for when the salt is missing in a request.
+	HashMissing = "Hash is missing" // Error message for when the hash is missing in a request.
+
+	UserLoginSuccess = "User log in success" // Success message for when the username and hash match in the requests.
+	SignupSuccess = "User added successfully" // Success message for when the user is successesfully added to the database.
+	SaltReturned = "Salt was returned" // Success message for when the users matching salt was successfully found in the database and returned.
 
 )
