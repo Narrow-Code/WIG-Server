fcb5a61bf4fce26173047463c52d81c9d4ac69bd
stitchy <matt.mccaughey91@gmail.com>
2023-11-22 10:33:23 -0500

    #48 fix middleware use utils.NewError

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,18 +1,20 @@
 /*
 * The middleware package handles all middle functions between the API calls.
-*/
+ */
 package middleware
 
 import (
-	"github.com/joho/godotenv"
-        "os"
-	"github.com/gofiber/fiber/v2"
 	"WIG-Server/messages"
+	"WIG-Server/utils"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 /*
 * AppAuthHeaderCheck checks that the AppAuth header is valid.
-*/
+ */
 func AppAuthHeaderCheck() fiber.Handler {
 	// Get AppAuth secret
 	godotenv.Load()
@@ -21,10 +23,7 @@ func AppAuthHeaderCheck() fiber.Handler {
 		headerValue := c.Get("AppAuth")
 		
 		if headerValue != os.Getenv("APP_SECRET"){
-			return c.Status(400).JSON(
-				fiber.Map{
-					"success":false,
-					"message":messages.AccessDenied})
+			return utils.NewError(c, 400, messages.AccessDenied)
 		}
 		
 		return c.Next()
