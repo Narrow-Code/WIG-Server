1fc8eac6b52ed6de77cf07573ac7d81faf41a77c
stitchy <matt.mccaughey91@gmail.com>
2023-12-03 15:45:07 -0500

    #62 remove salt and hash from user response

--- models/userModel.go
+++ models/userModel.go
@@ -5,8 +5,8 @@ type User struct {
 	UserUID      uint   `json:"userUID" gorm:"primary_key;column:user_uid"`
 	Username     string `json:"username" gorm:"column:username"`
 	Email        string `json:"email" gorm:"column:email"`
-	Salt         string `json:"salt" gorm:"column:salt"`
-	Hash         string `json:"hash" gorm:"column:hash"`
+	Salt         string `json:"-" gorm:"column:salt"`
+	Hash         string `json:"-" gorm:"column:hash"`
 	EmailConfirm string `json:"emailConfirmed" gorm:"column:email_confirm;default:false"`
 	Tier	     string `json:"tier" gorm:"column:tier"`
 	Token        string `json:"-" gorm:"column:token"`
