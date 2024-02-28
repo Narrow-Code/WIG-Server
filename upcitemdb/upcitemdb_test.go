6020b24bb8fd5de735714fe67c0a276ecfd0aa8e
stitchy <matt.mccaughey91@gmail.com>
2024-02-28 15:40:56 -0500

    Fix test case for upcitemdb.go

--- upcitemdb/upcitemdb_test.go
+++ upcitemdb/upcitemdb_test.go
@@ -1,12 +1,20 @@
 package upcitemdb
 
 import (
+	"WIG-Server/db"
 	"testing"
-
+	"os"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetBarcodePossitive(t *testing.T) {
-	value := GetBarcode("1234")
+func TestGetBarcodePossitive(t *testing.T) {	
+	os.Setenv("MYSQL_DBNAME", "wig")
+	os.Setenv("MYSQL_USER", "wig")
+	os.Setenv("MYSQL_PASSWORD", "wigsecret")
+	os.Setenv("MYSQL_HOST", "localhost:3306")
+	
+	db.Connect()
+	value := GetBarcode("305212345001")
+	//value := GetBarcode("1234")
 	assert.Equal(t, value, 0)
 }
