7ec6ea3e218ad93f48b1025c236066b872477f62
stitchy <matt.mccaughey91@gmail.com>
2024-04-23 19:12:34 -0400

    Add comments to Location structs

--- models/locationModel.go
+++ models/locationModel.go
@@ -2,15 +2,32 @@ package models
 
 import "github.com/google/uuid"
 
-// Represents information about a location.
+// Location represents information about a location.
 type Location struct {
-	LocationUID         uuid.UUID      `json:"locationUID" gorm:"primary_key;column:location_uid;type:varchar(191)"`
-	LocationOwner       uuid.UUID      `json:"locationOwner" gorm:"column:location_owner;type:varchar(191)"`
-	LocationName        string    `json:"locationName" gorm:"column:location_name"`
-	Parent              uuid.UUID     `json:"locationParent" gorm:"column:location_parent;type:varchar(191);default:AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA"`
-	LocationQR          string    `json:"locationQR" gorm:"column:location_qr"`
-	LocationTags        string    `json:"locationTags" gorm:"column:location_tags"`
-	LocationDescription string    `json:"locationDescription" gorm:"column:location_description"`
-	User                User      `json:"user" gorm:"foreignkey:location_owner"`
-	Location            *Location `json:"location" gorm:"foreignkey:location_parent"`
+	// LocationUID uniquely identifies the location
+	LocationUID uuid.UUID `json:"locationUID" gorm:"primary_key;column:location_uid;type:varchar(191)"`
+
+	// LocationOwner is the UUID of the user who owns the location
+	LocationOwner uuid.UUID `json:"locationOwner" gorm:"column:location_owner;type:varchar(191)"`
+
+	// LocationName is the name of the location
+	LocationName string `json:"locationName" gorm:"column:location_name"`
+	
+	// Parent is the parent location in which the location is located in
+	Parent uuid.UUID `json:"locationParent" gorm:"column:location_parent;type:varchar(191);default:AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA"`
+	
+	//LocationQR is the QR code associated with the location
+	LocationQR string `json:"locationQR" gorm:"column:location_qr"`
+	
+	// LocationTags are all of the tags representing the location, seperated by commas
+	LocationTags string `json:"locationTags" gorm:"column:location_tags"`
+	
+	// LocationDescription is a custom description for the location
+	LocationDescription string `json:"locationDescription" gorm:"column:location_description"`
+	
+	// User is the user account associated with the location
+	User User `json:"user" gorm:"foreignkey:location_owner"`
+	
+	// Location is the parent location associated with the location
+	Location *Location `json:"location" gorm:"foreignkey:location_parent"`
 }
