3c9c7edf4a4719d12ed15fdcec88306ee000031f
stitchy <matt.mccaughey91@gmail.com>
2024-04-23 09:41:27 -0400

    Fix location UID to use UUID format

--- models/locationModel.go
+++ models/locationModel.go
@@ -1,11 +1,13 @@
 package models
 
+import "github.com/google/uuid"
+
 // Represents information about a location.
 type Location struct {
-	LocationUID         uint      `json:"locationUID" gorm:"primary_key;column:location_uid"`
+	LocationUID         uuid.UUID      `json:"locationUID" gorm:"primary_key;column:location_uid;type:varchar(191)"`
 	LocationOwner       uint      `json:"locationOwner" gorm:"column:location_owner"`
 	LocationName        string    `json:"locationName" gorm:"column:location_name"`
-	Parent              *uint     `json:"locationParent" gorm:"column:location_parent;default:1"`
+	Parent              *uuid.UUID     `json:"locationParent" gorm:"column:location_parent;type:varchar(191);default:44444444-4444-4444-4444-444444444444"`
 	LocationQR          string    `json:"locationQR" gorm:"column:location_qr"`
 	LocationTags        string    `json:"locationTags" gorm:"column:location_tags"`
 	LocationDescription string    `json:"locationDescription" gorm:"column:location_description"`
