c2c0212270981972b52e493dd73006251d28c24c
stitchy <matt.mccaughey91@gmail.com>
2024-04-24 14:17:52 -0400

    Add checkinItems function

--- controller/borrowerFunctions.go
+++ controller/borrowerFunctions.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/uuid"
 )
 
+// createBorrower creates a models.Borrower and adds it to the database
 func createBorrower(borrowerName string, user models.User) models.Borrower{
 	borrower := models.Borrower{
 		BorrowerName:  borrowerName,
@@ -19,6 +20,7 @@ func createBorrower(borrowerName string, user models.User) models.Borrower{
 	return borrower
 }
 
+// checkoutItems takes a list of ownership UUID's and checks them out to a single borrower
 func checkoutItems(ownerships []string, borrowerUUID uuid.UUID) []string{
 	var successfulOwnerships []string
 	for _, ownership := range ownerships {		
@@ -28,6 +30,26 @@ func checkoutItems(ownerships []string, borrowerUUID uuid.UUID) []string{
 		_, err := RecordExists("Ownership", result)
 		if err == nil {
 			item.ItemBorrower = borrowerUUID
+			item.ItemCheckedOut = "true"
+			db.DB.Save(&item)
+			preloadOwnership(&item)
+			successfulOwnerships = append(successfulOwnerships, ownership)
+		}
+	}
+	return successfulOwnerships
+}
+
+// checckinItems takes a list of ownership UUID's and returns them to the original location
+func checkinItems(ownerships []string) []string {
+	var successfulOwnerships []string
+	for _, ownership := range ownerships{		
+		var item models.Ownership
+		result := db.DB.Where("ownership_uid = ?", ownership).First(&item)
+		
+		_, err := RecordExists("Ownership", result)
+		if err == nil {
+			item.ItemBorrower = uuid.MustParse("11111111-1111-1111-1111-111111111111")
+			item.ItemCheckedOut = "false"
 			db.DB.Save(&item)
 			preloadOwnership(&item)
 			successfulOwnerships = append(successfulOwnerships, ownership)
