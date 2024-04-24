0fa248852e5f2bf78d91908ff717ccc6c317ceff
stitchy <matt.mccaughey91@gmail.com>
2024-04-24 09:46:07 -0400

    Add comments to Ownership structs

--- models/ownershipModel.go
+++ models/ownershipModel.go
@@ -4,22 +4,53 @@ import(
 	"github.com/google/uuid"
 )
 
-// Represents information about ownership.
+// Ownership represents information about ownership.
 type Ownership struct {
-	OwnershipUID   uuid.UUID     `json:"ownershipUID" gorm:"primary_key;column:ownership_uid;type:varchar(191)"`
-	ItemOwner      uuid.UUID     `json:"itemOwner" gorm:"column:item_owner;type:varchar(191)"`
-	ItemNumber     uuid.UUID     `json:"itemNumber" gorm:"column:item_number;type:varchar(191)"`
-	CustomItemName string   `json:"customItemName" gorm:"column:custom_item_name"`
-	CustItemImg    string   `json:"customItemImage" gorm:"column:custom_item_img"`
-	OwnedCustDesc  string   `json:"customItemDescription" gorm:"column:custom_item_description"`
-	ItemLocation   uuid.UUID     `json:"itemLocation" gorm:"column:item_location;default:AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA;type:varchar(191)"`
-	ItemQR         string   `json:"itemQR" gorm:"column:item_qr"`
-	ItemTags       string   `json:"itemTags" gorm:"column:item_tags"`
-	ItemQuantity   int      `json:"itemQuantity" gorm:"column:item_quantity;default:1"`
-	ItemCheckedOut string   `json:"itemCheckedOut" gorm:"column:item_checked_out"`
-	ItemBorrower   uuid.UUID     `json:"itemBorrower" gorm:"column:item_borrower;type:varchar(191);default:11111111-1111-1111-1111-111111111111"`
-	User           User     `json:"user" gorm:"foreignkey:item_owner"`
-	Location       Location `json:"location" gorm:"foreignkey:item_location"`
-	Item           Item     `json:"item" gorm:"foreignkey:item_number"`
-	Borrower       Borrower `json:"borrower" gorm:"foreignkey:item_borrower"`
+	// OwnershipUID uniquely identifies the ownership
+	OwnershipUID uuid.UUID `json:"ownershipUID" gorm:"primary_key;column:ownership_uid;type:varchar(191)"`
+
+	// ItemOwner is the UUID of the user who owns the ownership  
+	ItemOwner uuid.UUID `json:"itemOwner" gorm:"column:item_owner;type:varchar(191)"`
+
+	// ItemNumber is the UUID of the item
+	ItemNumber uuid.UUID `json:"itemNumber" gorm:"column:item_number;type:varchar(191)"`
+
+	// CustomItemName is the users custom name for the ownership
+	CustomItemName string `json:"customItemName" gorm:"column:custom_item_name"`
+
+	// CustItemImg is a link to the image to be used to represent the ownership item
+	CustItemImg string `json:"customItemImage" gorm:"column:custom_item_img"`
+
+	// OwnedCustDesc is the users custom description for the ownership
+	OwnedCustDesc string `json:"customItemDescription" gorm:"column:custom_item_description"`
+	
+	// ItemLocation is the UUID of the location in which the ownership is stored
+	ItemLocation uuid.UUID `json:"itemLocation" gorm:"column:item_location;default:AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA;type:varchar(191)"`
+	
+	// ItemQR is the QR code representing the ownership
+	ItemQR string `json:"itemQR" gorm:"column:item_qr"`
+	
+	// ItemTags are all of the tags representing the ownership, seperated by commas 
+	ItemTags string `json:"itemTags" gorm:"column:item_tags"`
+	
+	// ItemQuantity is the inventory quantity of the ownership
+	ItemQuantity int `json:"itemQuantity" gorm:"column:item_quantity;default:1"`
+	
+	// ItemCheckedOut is a boolean expression detailing if the item is checked out or borrowed
+	ItemCheckedOut string `json:"itemCheckedOut" gorm:"column:item_checked_out"`
+	
+	// ItemBorrower is the UUID of the borrower who the ownership is checked out to
+	ItemBorrower uuid.UUID `json:"itemBorrower" gorm:"column:item_borrower;type:varchar(191);default:11111111-1111-1111-1111-111111111111"`
+	
+	// User is the user account associated with the ownership
+	User User `json:"user" gorm:"foreignkey:item_owner"`
+	
+	// Location is the location associated with the ownership
+	Location Location `json:"location" gorm:"foreignkey:item_location"`
+	
+	// Item is the item associated with the ownership
+	Item Item `json:"item" gorm:"foreignkey:item_number"`
+	
+	// Borrower is the borrower associated with the ownership
+	Borrower Borrower `json:"borrower" gorm:"foreignkey:item_borrower"`
 }
