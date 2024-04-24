e73e7f8ec6da8ac8e54d9711295cb64766d68b0d
stitchy <matt.mccaughey91@gmail.com>
2024-04-24 14:10:23 -0400

    Add checkoutItems function

--- controller/borrowerFunctions.go
+++ controller/borrowerFunctions.go
@@ -18,3 +18,20 @@ func createBorrower(borrowerName string, user models.User) models.Borrower{
 
 	return borrower
 }
+
+func checkoutItems(ownerships []string, borrowerUUID uuid.UUID) []string{
+	var successfulOwnerships []string
+	for _, ownership := range ownerships {		
+		var item models.Ownership
+		result := db.DB.Where("ownership_uid = ?", ownership).First(&item)
+		
+		_, err := RecordExists("Ownership", result)
+		if err == nil {
+			item.ItemBorrower = borrowerUUID
+			db.DB.Save(&item)
+			preloadOwnership(&item)
+			successfulOwnerships = append(successfulOwnerships, ownership)
+		}
+	}
+	return successfulOwnerships
+}
