7fb37be20a0dd3e585c7a42574cf35e3f9775218
stitchy <matt.mccaughey91@gmail.com>
2024-04-25 12:13:18 -0400

    Refactor OwnershipDelete for readability

--- controller/ownershipController.go
+++ controller/ownershipController.go
@@ -58,34 +58,25 @@ func OwnershipQuantity(c *fiber.Ctx) error {
 	return success(c, "Item found", dto)
 }
 
-/*
-* Deletes an ownership from the database.
-*
-* @param c The Fiber context containing the HTTP request and response objects.
-*
-* @return error The error message, if there is any.
- */
+// OwnershipDelete deletes an ownership from the database.
 func OwnershipDelete(c *fiber.Ctx) error {
 	// Initialize variables
+	var ownership models.Ownership
 	user := c.Locals("user").(models.User)
 	ownershipUID := c.Query("ownershipUID")
 
 	// Validate ownership
-	var ownership models.Ownership
 	result := db.DB.Where("ownership_uid = ? AND item_owner = ?", ownershipUID, user.UserUID).First(&ownership)
 	code, err := recordExists(result)
 	if err != nil {
 		return Error(c, code, err.Error())
 	}
 
+	// Delete ownership and return
 	db.DB.Delete(&ownership)
-
-	// Check for errors after the delete operation
 	if result := db.DB.Delete(&ownership); result.Error != nil {
 		return Error(c, 500, "There was an error deleting the ownership")
 	}
-
-	// Ownership successfully deleted
 	return success(c, "Ownership was successfully deleted")
 }
 
