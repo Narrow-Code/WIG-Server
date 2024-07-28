c4c313c1a47678c74417fdddf9a258d91c7b13ad
stitchy <matt.mccaughey91@gmail.com>
2024-07-27 23:34:02 -0400

    add SendResetPasswordEmail to email.go

--- utils/email.go
+++ utils/email.go
@@ -38,3 +38,34 @@ func SendVerificationEmail(receiver string, username string) {
     fmt.Println("Email sent successfully!")
 }
 
+func SendResetPasswordEmail(receiver string, username string) {
+    // TODO generate reset password token & link
+    resetLink := "http://narrowcode.org"
+
+    sender := os.Getenv("EMAIL")
+    pass := os.Getenv("EMAIL_PASS")
+    host := os.Getenv("EMAIL_HOST")
+    port := os.Getenv("EMAIL_PORT")
+
+    auth := smtp.PlainAuth("", sender, pass, host)
+
+    // The message to send.
+    to := []string{receiver}
+    msg := []byte("To: " + receiver + "\r\n" +
+        "Subject: WIG Verification\r\n" +
+        "\r\n" +
+        "Hello " + username + ",\r\n\r\n" +
+        "You have requested to reset your password for our WIG service.\r\n\r\n" +
+        "If you would like to reset your password please click the following link: " + resetLink + "\r\n\r\n" +
+        "Thanks, \r\n\r\n" +
+        "Narrow Code")
+
+    // Send the email.
+    err := smtp.SendMail(host + ":" + port, auth, sender, to, msg)
+    if err != nil {
+        fmt.Println("Failed to send email:", err)
+        return
+    }
+
+    fmt.Println("Email sent successfully!")
+}
